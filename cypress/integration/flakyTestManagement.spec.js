05d3a1f4aaf3503583db548accb10c2ba8c889e2
Walmyr <wlsf82@gmail.com>
2022-05-13 23:33:31 +0200

    Remove code duplication

--- cypress/integration/flakyTestManagement.spec.js
+++ cypress/integration/flakyTestManagement.spec.js
@@ -1,14 +1,14 @@
 describe('Flaky test managment', () => {
+  beforeEach(() => cy.visit('./index.html'))
+
   Cypress._.times(10, index => {
     it(`${index + 1} - waits for paragraph to appear`, () => {
-      cy.visit('./index.html')
       cy.contains('p', 'I finally appeared.', { timeout: 10100})
     })
   })
 
   Cypress._.times(3, index => {
     it(`${index + 1} - waits for heading 1 to appear`, () => {
-      cy.visit('./index.html')
       cy.contains('h1', 'Flaky Test Management')
     })
   })
